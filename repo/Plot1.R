a7434cfa294773de25acc6ecbf14d51970c44388
RajagopalanK <rajagopalan_1979@Yahoo.com>
2015-03-07 13:56:00 +0530

    Assignment

---
+++ repo/Plot1.R
@@ -0,0 +1,7 @@
+setwd("c:/r/data")
+d=read.table("household_power_consumption.txt",sep=";",header=T,stringsAsFactors=F, na.strings="?")
+s=subset(d,Date=="1/2/2007"|Date=="2/2/2007")
+s$Global_active_power=as.numeric(s$Global_active_power)
+hist(s$Global_active_power, col="red", xlab="Global Active Power (kilowatts)",main="Global Active Power")
+dev.copy(png, file="Plot1.png", bg="white")
+dev.off()
