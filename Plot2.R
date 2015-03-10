bda540e30f44a7977762a1c4b7cfe0ba2acb02e6
lamtzehau <lamtzehau@hotmail.com>
2015-03-11 00:13:02 +0800

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -4,8 +4,8 @@ s$Global_active_power=as.numeric(s$Global_active_power)
 
 s$Time=paste(s$Date, s$Time)
 s$Time=strptime(s$Time, "%d/%m/%Y %H:%M:%S")
-s$Time=format(s$Time,format="%H/%M/%S")
-s <- strptime(paste(s$Date, s$Time), "%d/%m/%Y %H:%M") 
+s$Time=format(s$Time,format="%H/%M/%S")  # redundant code line, previous line already converted the s$Time to the requireed data and time formaat
+s <- strptime(paste(s$Date, s$Time), "%d/%m/%Y %H:%M")  # remove this line, code would result in "NA" list and hence the subsequent codes are not going to work ie cant create the plot 
 
 png(file="Plot2.png",width=480,height=480,units="px")	
 plot(s$Time,s$Global_active_power, pch=".",ylab="Global Active Power (Kilowatts)", xlab="")
